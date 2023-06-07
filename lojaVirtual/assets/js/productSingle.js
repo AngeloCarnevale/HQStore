9da4250f6c0255cc1b97a9f12b1b9f6bc35229cc
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-06-07 16:49:47 -0300

    Adicionando bootstrap

--- lojaVirtual/assets/js/productSingle.js
+++ lojaVirtual/assets/js/productSingle.js
@@ -1,16 +1,18 @@
+var desc_produto = document.getElementById('infos-produto')
+var link = document.getElementsByTagName('a')
 lista = new Array
 var carrinho = JSON.parse(localStorage.getItem('carrinho'))
+var produtoUnico = JSON.parse(localStorage.getItem('produto'))
+
 if(carrinho == null){
     localStorage.setItem('carrinho', JSON.stringify(lista))
 }
-
-var productSingle = document.getElementById('productSingle')
 
 exibirProduto = () => {
     
     var produto = JSON.parse(localStorage.getItem('produto'))
 
-    productSingle.innerHTML += `
+    desc_produto.innerHTML += `
     
         <div class="info-produto">
             <img src="`+produto.img+`"/>
@@ -26,19 +28,46 @@ exibirProduto = () => {
 exibirProduto()
 
 atualizarCarrinho = (id, nome, preco, img) => {
-    
-    let produto = {
-        id: id,
-        nome: nome,
-        preco: preco,
-        img: img,
-        quantidade: 1
+
+    var ids = []
+    for(var i = 0; i < carrinho.length; i++){
+        ids.push(carrinho[i].id)
     }
 
-    carrinho.push(produto)
-    localStorage.setItem('carrinho', JSON.stringify(carrinho))
+    if(carrinho == ''){
+        let produto = {
+            id: id,
+            nome: nome,
+            preco: preco,
+            img: img,
+            quantidade: 1
+        }
+        carrinho.push(produto)
+        localStorage.setItem('carrinho', JSON.stringify(carrinho))
+    }
+    else {
+        if(ids.includes(id)){
+            for(var i = 0; i < carrinho.length; i++){
+                if(carrinho[i].id == id) {
+                    carrinho[i].quantidade++
+                    localStorage.setItem('carrinho', carrinho)
+                }
+            }
+        }
+        else {
+            let produto = {
+                id: id,
+                nome: nome,
+                preco: preco,
+                img: img,
+                quantidade: 1
+            }
+            carrinho.push(produto)
+        }
+} 
+    localStorage.setItem('carrinho', JSON.stringify(carrinho))   
+    localStorage.setItem('produto', JSON.stringify(produtoUnico))
     
+    localStorage.removeItem('produto')
 alert('Produto adicionado com sucesso!')
-    
-    
 }  
